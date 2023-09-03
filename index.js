06df42f594a01627495ecfa6dbdc83c174a9fea0
Erick Velasco <erickv_24@yahoo.com>
2023-09-03 11:44:58 -0400

    getting API ready to deploy and added new ENDPOINT for single post

--- index.js
+++ index.js
@@ -1,37 +1,63 @@
-import express from 'express'
-import cors from 'cors'
-import { MongoClient, ObjectId } from 'mongodb'
-import 'dotenv/config'
+import express from "express"
+import cors from "cors"
+import { MongoClient, ObjectId } from "mongodb"
+import "dotenv/config"
 
+const URI = process.env.MONGO_URI
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-const client = new MongoClient(process.uri.MONGOURI)
-const port = process.env.PORT
-const db = client.db('blog')
-const posts = db.collection('posts')
+const client = new MongoClient(URI)
+const PORT = process.env.PORT
+const db = client.db("blog")
+const posts = db.collection("posts")
 
-app.get('/', async (req, res) => {
-    const allPosts = await posts.find().toArray()
-    res.send(allPosts)
+// API ENDPOINT to route to a single post
+app.get("/single-post/:id", async (req, res) => {
+  //
+  const specialId = new ObjectId(req.params.id)
+  console.log(specialId)
+  console.log("req.params ->", req.params)
+
+  // get the id
+  // find the item by the id
+  // send back the item matching the id
+  const singlePost = await posts.findOne({ _id: specialId })
+  console.log(singlePost)
+
+  res.send(singlePost)
 })
 
-app.post('/', async (req, res) => {
-    console.log(req.body)
-
-    const addPost = await posts.insertOne(req.body)
-    res.send(addPost)
+// API ENDPOINT to Home - to show all the posts
+app.get("/", async (req, res) => {
+  const allPosts = await posts.find().toArray()
+  res.send(allPosts)
 })
 
-app.patch('/', (req, res) => {
-    console.log('Im patching')
-    res.send('Im patching')
+// ENDPOINT to Update posts
+app.post("/", async (req, res) => {
+  console.log(req.body)
+
+  const addPost = await posts.insertOne(req.body)
+  res.send(addPost)
 })
 
-app.delete('/', (req, res) => {
-    console.log('Im deleting')
-    res.send('Im deleting')
+app.patch("/", async (req, res) => {
+  const id = new ObjectId(req.body._id)
+
+  const patchPost = await posts.findOneAndUpdate(
+    { _id: id },
+    { $set: req.body }
+  )
+  res.send(patchPost)
 })
 
-app.listen(4040, () => console.log('Im running on 4040'))
+app.delete("/", async (req, res) => {
+  const id = new ObjectId(req.body._id)
+
+  const deletPost = await posts.findOneAndDelete({ _id: id })
+  res.send(deletPost)
+})
+
+app.listen(PORT, () => console.log("Im running on 4040"))
